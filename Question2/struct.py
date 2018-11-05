919a06d0fb05716296a0b25815e67562dd9d4d31
GabrielGelgor <gabriel.gelgor@uoit.net>
2018-11-04 22:10:04 -0500

    Created needed data structures

---
+++ Question2/struct.py
@@ -0,0 +1,9 @@
+class Sum:
+    __init__(_list, value):
+        self._list = _list
+        self.val = value
+
+class node:
+    __init__(value, status="open"):
+        self.val = value
+        self.status = status
