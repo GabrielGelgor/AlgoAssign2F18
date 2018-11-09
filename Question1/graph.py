fee4737ad0d9ee85ac90d8a77c849131bc6d3513
Umar Ehsan <umarehsan@gmail.com>
2018-11-09 11:24:18 -0500

    Added pseudocode and graph class for question 1

---
+++ Question1/graph.py
@@ -0,0 +1,26 @@
+class Graph():
+    def __init__(self, n):
+        self.n = n
+	self.adjMatrix = []
+	for i in range (0,n):
+		self.adjMatrix.append([0]*n)
+
+    def BFSpartition(self, i, AssignmentList):
+	adjList = []
+	adjList.append(i)
+
+	AssignmentList[i] = 1
+
+	while (len(adjList) > 0):
+		root = adjList.pop(0)
+
+		for vertex in range(0,n):
+			if (root == vertex) continue
+
+			else if (AssignmentList[root] == AssignmentList[vertex]):
+				return False
+
+			else if (adjMatrix[root][vertex] == 0):
+				AssignmentList[vertex] = 3 - AssignmentList[root]
+				adjList.append(vertex)
+	return True
