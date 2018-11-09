7131cac5d9ea13bd8f3cd081c457e5d3106badff
Umar Ehsan <umarehsan@gmail.com>
2018-11-09 18:08:38 -0500

    Fixed indentations in graph.py

--- Question1/graph.py
+++ Question1/graph.py
@@ -1,26 +1,26 @@
 class Graph():
     def __init__(self, n):
         self.n = n
-	self.adjMatrix = []
-	for i in range (0,n):
-		self.adjMatrix.append([0]*n)
+        self.adjMatrix = []
+        for i in range (0,n):
+            self.adjMatrix.append([0]*n)
 
     def BFSpartition(self, i, AssignmentList):
-	adjList = []
-	adjList.append(i)
+        adjList = []
+        adjList.append(i)
 
-	AssignmentList[i] = 1
+        AssignmentList[i] = 1
 
-	while (len(adjList) > 0):
-		root = adjList.pop(0)
+        while (len(adjList) > 0):
+            root = adjList.pop(0)
 
-		for vertex in range(0,n):
-			if (root == vertex) continue
+            for vertex in range(0,n):
+                if (root == vertex) continue
 
-			else if (adjMatrix[root][vertex] == 0 and AssignmentList[root] == AssignmentList[vertex]):
-				return False
+                else if (adjMatrix[root][vertex] == 0 and AssignmentList[root] == AssignmentList[vertex]):
+                    return False
 
-			else if (adjMatrix[root][vertex] == 0 and AssignmentList[root] != AssignmentList[vertex]):
-				AssignmentList[vertex] = 3 - AssignmentList[root]
-				adjList.append(vertex)
-	return True
+                else if (adjMatrix[root][vertex] == 0 and AssignmentList[root] != AssignmentList[vertex]):
+                    AssignmentList[vertex] = 3 - AssignmentList[root]
+                    adjList.append(vertex)
+        return True
